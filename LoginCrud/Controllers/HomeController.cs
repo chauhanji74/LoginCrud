b0b5f834d69aa5473d9ce9f71132c4c017ee6b9c
user <lalitsinghchauhan74@gmail.com>
2022-07-12 00:20:21 +0530

    Add project files.

---
+++ LoginCrud/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginCrud.DbCon;
+using System.Data.Entity;
+
+namespace LoginCrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        LoginCrudEntities db = new LoginCrudEntities();
+        // GET: Home
+        [Authorize]
+        public ActionResult Index()
+        {
+            var emp = db.tbl_employee.ToList();
+            return View(emp);
+        }
+
+        [Authorize]
+        public ActionResult Create(int id = 0)
+        {
+            if(id == 0)
+            {
+                TempData["Title"] = "Add Employee";
+            }
+            else
+            {
+                var udata = db.tbl_employee.Where(m => m.id == id).FirstOrDefault();
+                TempData["Title"] = "Update";
+                return View(udata);
+            }
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(tbl_employee emp)
+        {
+            if (emp.id == 0)
+            {
+                
+                db.tbl_employee.Add(emp);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["AMessage"] = "<script>alert('Added Successfully...')</script>";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["AMessage"] = "<script>alert('Not Added...')</script>";
+                }
+                
+            }
+            else
+            {
+                db.Entry(emp).State = EntityState.Modified;
+                int b = db.SaveChanges();
+                if (b > 0)
+                {
+                    TempData["UMessage"] = "<script>alert('Updated Successfully...')</script>";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["UMessage"] = "<script>alert('Not Updated...')</script>";
+                }
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            var delItem = db.tbl_employee.Where(m => m.id == id).FirstOrDefault();
+            db.tbl_employee.Remove(delItem);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ LoginCrud/Controllers/LoginController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginCrud.DbCon;
+using System.Web.Security;
+
+namespace LoginCrud.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginCrudEntities db = new LoginCrudEntities();
+        // GET: Login
+        public ActionResult Signin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signin(tbl_user si)
+        {
+            var data = db.tbl_user.Where(m => m.email == si.email).FirstOrDefault();
+            if(data != null)
+            {
+                if(data.password == si.password)
+                {
+                    FormsAuthentication.SetAuthCookie(si.email, false);
+                    Session["m"] = "hi";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Wrong Password";
+                }
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Invalid Email";
+            }
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(tbl_user su)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.tbl_user.Add(su);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["RMessage"] = "<script>alert('Registered Successfully...')</script>";
+                    return RedirectToAction("Signin", "Login");
+                }
+                else
+                {
+                    TempData["RMessage"] = "<script>alert('Not Registered...')</script>";
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session["m"] = "bye";
+            return RedirectToAction("Signin", "Login");
+        }
+    }
+}
